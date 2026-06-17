5df2f1a1daeaf8de3cc20952fd3501114533adbe
harishtpj <harishtpj@hotmail.com>
2026-06-17 12:24:03 +0530

    Added Route Guards

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\SessionController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
@@ -13,14 +13,16 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('logout', [SessionController::class, 'destroy'])->name('logout');
 });
 
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [SessionController::class, "create"])->name('login');
+    Route::post('/login', [SessionController::class, "store"]);
+
+    Route::get('/register', [RegistrationController::class, "create"])->name('register');
+    Route::post('/register', [RegistrationController::class, "store"]);
+});
+
 Route::get('/', function () {
     return Inertia::render('Home', [
         'name' => request('name', 'World')
     ]);
 })->name('index');
-
-Route::get('/login', [SessionController::class, "create"])->name('login');
-Route::post('/login', [SessionController::class, "store"]);
-
-Route::get('/register', [RegistrationController::class, "create"])->name('register');
-Route::post('/register', [RegistrationController::class, "store"]);
