0e5eb3bdc008995d141a08a5e5f0219188c47bfe
harishtpj <harishtpj@hotmail.com>
2026-06-19 11:55:21 +0530

    Revert "prepare for deploying"
    This reverts commit c296d4a61cd22f93e20e704a2cc77c67c3cdde6c.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Inertia\ExceptionResponse;
@@ -21,10 +20,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url): void
+    public function boot(): void
     {
         if (App::environment('production')) {
-            $url->forceHttps();
             Inertia::handleExceptionsUsing(function (ExceptionResponse $resp) {
                 if (in_array($resp->statusCode(), [403, 404, 500, 503])) {
                     return $resp->render('ErrorPage', [
